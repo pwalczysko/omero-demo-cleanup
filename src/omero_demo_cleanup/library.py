f87b9ab3f795559e63e3f12f1dc6f2a89440b6b9
pwalczysko <p.walczysko@dundee.ac.uk>
2025-12-19 15:53:33 +0000

    try

--- src/omero_demo_cleanup/library.py
+++ src/omero_demo_cleanup/library.py
@@ -333,9 +333,8 @@ def resource_usage(
                 if who.first == user_id:
                     file_size += usage
         except Exception as ex:
-            print(
-                f'FAILED to count data for "{user_name}" (#{user_id}), error message is {ex}.'
-            )
+            logging.error("Critical error in process!", exc_info=True) # Logs traceback
+
 
         if file_count > 0 or file_size > 0:
             user_stats.append(
